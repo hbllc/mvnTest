842c1e3b378b4c158e09c16b1bb727cb821ea6a8
lilichuan <lilichuan@microconnect.com>
2023-08-17 15:49:12 +0800

    add:sort

--- src/main/java/com/llc/test/StreamTest.java
+++ src/main/java/com/llc/test/StreamTest.java
@@ -1,8 +1,6 @@
 package com.llc.test;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -80,6 +78,105 @@ public class StreamTest {
         List<Student> page = students.stream().skip(5).limit(5).collect(Collectors.toList());
         System.out.println(page);
         System.out.println("===========================");
+
+
+        // 按成绩排序，成绩相同时根据年龄排倒序
+        List<Student> sorted = students.stream()
+                .sorted(Comparator.comparing(Student::getScore, Comparator.reverseOrder())
+                        .thenComparing(Student::getAge, Comparator.reverseOrder()))
+                .collect(Collectors.toList());
+// 普通写法
+        List<Student> sorted2 = students.stream()
+                .sorted((c1, c2) -> {
+                    if (c1.getScore() > c2.getScore()) {
+                        return -1;
+                    } else if (c1.getScore().equals(c2.getScore())) {
+                        if (c1.getAge() > c2.getAge()) {
+                            return -1;
+                        } else if (c1.getAge() < c2.getAge()) {
+                            return 1;
+                        }
+                        return 0;
+                    }
+                    return 1;
+                })
+                .collect(Collectors.toList());
+        System.out.println(sorted);
+        System.out.println(sorted2);
+
+        // 分数的最大值 最小值
+        Student max = students.stream().max(Comparator.comparing(Student::getScore)).orElse(null);
+        Student min = students.stream().min(Comparator.comparing(Student::getScore)).orElse(null);
+// 也可以通过如下写法获取最大值，这种写法只能获取到值，没法关联用户，跟上面的写法各有用途
+        int max2 = students.stream().mapToInt(Student::getScore).max().orElse(0);
+        int min2 = students.stream().mapToInt(Student::getScore).min().orElse(0);
+        System.out.println(max);
+        System.out.println(min);
+
+
+        // 计数求及格的学生人数
+        long count = students.stream().filter(student -> student.getScore() > 60).count();
+// 求分数总和
+        Integer sum = students.stream().map(Student::getScore).reduce(Integer::sum).orElse(-1);
+        Integer sum2 = students.stream().mapToInt(Student::getScore).sum();
+// 分数的平均值
+        double average = students.stream().mapToDouble(Student::getScore).average().orElse(0D);
+        System.out.println(count + "-" + sum + "-" + average);
+
+
+        // 将不同课程的学生进行分类
+        HashMap<String, List<Student>> groupByCourse = (HashMap<String, List<Student>>) students.stream()
+                .collect(Collectors.groupingBy(Student::getCourse));
+// 上面的方法中，最终返回默认是HashMap，键值对中的值默认是ArrayList，可以通过下面的方法自定义返回结果、值的类型
+        HashMap<String, List<Student>> groupByCourse1 = students.stream()
+                .collect(Collectors.groupingBy(Student::getCourse, HashMap::new, Collectors.toList()));
+// 增加映射功能，将值设置为名字
+        HashMap<String, List<String>> groupMapping = students.stream()
+                .collect(Collectors.groupingBy(Student::getCourse, HashMap::new, Collectors.mapping(Student::getName, Collectors.toList())));
+// 增加合并函数，计算每科总分
+        HashMap<String, Integer> groupCalcSum = students.stream()
+                .collect(Collectors.groupingBy(Student::getCourse, HashMap::new, Collectors.reducing(0, Student::getScore, Integer::sum)));
+// 增加平均值计算
+        HashMap<String, Double> groupCalcAverage = students.stream()
+                .collect(Collectors.groupingBy(Student::getCourse, HashMap::new, Collectors.averagingDouble(Student::getScore)));
+        System.out.println(groupByCourse);
+        System.out.println(groupMapping);
+        System.out.println(groupCalcSum);
+        System.out.println(groupCalcAverage);
+
+        // 去重 统计所有科目
+        List<String> courses = students.stream().map(Student::getCourse).distinct().collect(Collectors.toList());
+        System.out.println(courses);
+        System.out.println("===========================");
+
+        boolean b = students.stream().anyMatch(student -> student.getScore() >= 60);
+        System.out.println(b);
+        boolean b1 = students.stream().allMatch(student -> student.getScore() >= 60);
+        System.out.println(b1);
+        System.out.println("===========================");
+
+        students.stream().takeWhile(student -> student.getScore() == 60).forEach(System.out::println);
+        System.out.println("===========================");
+        students.stream().dropWhile(student -> student.getScore() >= 60).forEach(System.out::println);
+
+        System.out.println("===========================");
+        List<Integer> numberList = Arrays.asList(11,1, 3, 5, 8, 10, 20, 35, 2, 5, 7);
+        numberList.stream().sorted().takeWhile(num -> num <= 10).forEach(System.out::println);
+        System.out.println("===========================");
+        numberList.stream().sorted().dropWhile(num -> num <= 10).forEach(System.out::println);
+        System.out.println("===========================");
+
+        students.stream().filter(student -> student.getScore() > 10).findFirst().ifPresent(System.out::println);
+        System.out.println("===========================");
+        students.stream().sorted(Comparator.comparing(Student::getAge)).filter(student -> student.getScore() >= 60).findAny().ifPresent(System.out::println);
+        System.out.println("===========================");
+        System.out.println("========并发=======");
+        students.stream().filter(student -> student.getScore() > 10).findAny().ifPresent(System.out::println);
+        students.parallelStream().filter(student -> student.getScore() > 10).findAny().ifPresent(System.out::println);
+        System.out.println("===========================");
+        students.stream().filter(student -> student.getScore() > 10).findFirst().ifPresent(System.out::println);
+        students.parallelStream().filter(student -> student.getScore() > 10).findFirst().ifPresent(System.out::println);
+
 
 
 
